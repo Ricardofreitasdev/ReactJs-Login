ee89a4ae4e4ff58139494c56ae8cd6f7fe7d3be2
Ricardo Freitas <ricardofreitas07@live.com>
2022-05-22 19:29:52 -0300

    add url api production

--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-graphql-teste.herokuapp.com",
+  uri: "https://api-node-graphql.herokuapp.com/",
   cache: new InMemoryCache({}),
 });
